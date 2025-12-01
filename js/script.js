60aa05901979ff7e9dfc52ba5c54752968b558df
ZoutX <oftzoutyt@gmail.com>
2025-12-01 22:03:49 +1300

    second

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+  openPopup();
+};
+
+// Function to open the popup
+function openPopup() {
+  document.getElementById('popupContainer').style.display = 'block';
+}
+
+// Function to close the popup
+function closePopup() {
+  document.getElementById('popupContainer').style.display = 'none';
+}
